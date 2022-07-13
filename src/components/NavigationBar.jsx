b2ffd1ce4018e84aca4161f00a0db974ec8bfb6f
Kakajohn <dim_kak@msn.com>
2022-07-13 12:23:27 +0300

    Update NavigationBar.jsx

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-// import 'bootstrap/dist/css/bootstrap.min.css'
+import '../sass/components/_navbar.css'
 const NavigationBar = () => {
   return (
     <header>
-      <header>
      <div class="topnav">
-        <a class="active" href="/">Home</a>
-        <a href="/med4u/articles">Articles</a>
-        <a href="/med4u/chat">Chat</a>
+        <Link class="active" to="/">Home</Link>
+        <Link to="/med4u/articles">Articles</Link>
+        <Link to="/med4u/chat">Chat</Link>
       </div>
-    </header>
     </header>
   );
 };

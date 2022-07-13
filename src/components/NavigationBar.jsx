285915abd12fc70432fa56163aca099ade0444c7
Kakajohn <dim_kak@msn.com>
2022-07-13 12:13:33 +0300

    Update NavigationBar.jsx

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -4,24 +4,13 @@ import { Link, useNavigate } from 'react-router-dom';
 const NavigationBar = () => {
   return (
     <header>
-      <nav class="navbar navbar-expand-lg navbar-light">
-        <div class="container-fluid" style={{backgroundColor:'#972e3d', position:'fixed', zIndex:'1030', marginTop:'1rem'}}>
-          <Link class="navbar-brand" to="/">Home</Link>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <Link class="nav-link active" aria-current="page" to="/med4u/articles">Articles</Link>
-              </li>
-              <li class="nav-item">
-                <Link class="nav-link active" aria-current="page" to="/med4u/chat">Chat Room</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <header>
+     <div class="topnav">
+        <a class="active" href="/">Home</a>
+        <a href="/med4u/articles">Articles</a>
+        <a href="/med4u/chat">Chat</a>
+      </div>
+    </header>
     </header>
   );
 };

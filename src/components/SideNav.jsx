43aa27e338cf1d1d70b9bd84f4397bafc4d50cfc
GKakaz <dim_kak@msn.com>
2022-10-12 11:50:28 +0300

    fixed sidenav links

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,5 +1,6 @@
 import React , { useRef,useEffect, useState } from 'react';
 import '../sass/components/_sidenav.scss'
+import { Link, useNavigate } from 'react-router-dom';
 import {FaPlusCircle, FaHome,FaBars,FaArchive,FaAngleDown,FaAngleUp, faPlussCircle } from "react-icons/fa";
 const SideNav = () => {
     let [see, setSee] = useState(false);
@@ -8,8 +9,6 @@ const SideNav = () => {
     let expand = () => {
         setSee(true);
         document.getElementById('expand').style.width = '250px'
-        document.getElementById('togg').style.visibility = 'collapse'
-        document.getElementById('main').style.marginLeft = '250px'
     }
 
     let collapse = () => {
@@ -28,13 +27,13 @@ const SideNav = () => {
 
   return (
     <> <p id='collapse'></p>
-    <span className='togg' id='expand test' onClick={expand}> <FaBars /></span>
+    <span className='togg' id='expand' onClick={expand}> <FaBars /></span>
    {see &&  (<div className="sidenav" id=''>
-        <a href="" onClick={collapse} className='close' id='collapse' style={{transform: "rotate(45deg)"}}><FaPlusCircle /></a>
-        <a href="/" id='option'><FaHome/></a>
-        <a href="/med4u/articles" id='option'>Articles <FaArchive /></a>
-        <a href="/med4u/chat" id='option'>Chat</a>
-        <a href="/med4u/Login" id='option'>Log out</a>
+        <Link to="" onClick={collapse} className='close' id='collapse' style={{transform: "rotate(45deg)"}}><FaPlusCircle /></Link>
+        <Link to="/" id='option'><FaHome/></Link>
+        <Link to="/med4u/articles" id='option'>Articles <FaArchive /></Link>
+        <Link to="/med4u/chat" id='option'>Chat</Link>
+        <Link to="/med4u/Login" id='option'>Log out</Link>
     </div>)}
     <div id='main'></div>
     </>

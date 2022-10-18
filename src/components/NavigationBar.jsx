0c2f9c01d3bb432f9678aac37b9b70ee46be208b
GKakaz <dim_kak@msn.com>
2022-10-18 12:50:31 +0300

    cleanup

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import '../sass/components/_navbar.scss';
 import '../sass/components/_accordion.scss'
 import {FaPlusCircle, FaHome,FaBars,FaArchive,FaArrowRight,FaCommentAlt } from "react-icons/fa";
+import infos from '../config/navbar.json'
 const NavigationBar = () => {
 
   const [navbarOpen, setNavBarOpen] = useState(false);
@@ -16,18 +17,18 @@ const NavigationBar = () => {
       <nav className="topnav" >
           <button onClick={handleToggle}>{navbarOpen ? <FaPlusCircle id='nv icon' /> : <FaBars id='nv'/>}</button>          
             <div className={`menuNav ${navbarOpen ? "showMenu" : ""}`}>
-               <button onClick={handleToggle}>{navbarOpen ? <FaPlusCircle id='icon' /> : <FaBars />}</button>        
-                <ul>
-                  <li><Link id='option'to="/"><span><FaHome/> Home</span></Link></li>
-                  <li><Link id='option' to="/med4u/articles" ><FaArchive /> Articles </Link></li>
-                  <li><Link id='option' to="/med4u/chat" ><FaCommentAlt /> Chat</Link></li>
-                  <li><Link id='option' to="/med4u/Login" ><FaArrowRight/> Log out</Link></li>
-                  <li><Link id='option' to="/">Telemedicine</Link></li>
-                  <li><Link id='option' to="/med4u/articles">Distance</Link></li>
-                  <li><Link id='option' to="/med4u/chat">Autoimmune</Link></li>
-                </ul>
-
-          </div>
+             <button onClick={handleToggle}>{navbarOpen ? <FaPlusCircle id='icon' /> : <FaBars />}</button>        
+              <ul> 
+                {infos.NavInfos.map((item,index)=>{
+                    return(  
+                    <>
+                      <h3 id='pagination'>{item.helper}</h3>
+                      <li key={index} id='option'><Link to={item.link}>{item.name}</Link></li>
+                    </>  
+                  )
+                  })}
+               </ul>
+           </div>
         </nav>
     </>   
   );
